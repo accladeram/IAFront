1d63669945b2bd4cc22b6febf2456bfa4c18e01f
accladera <accladeram@gmail.com>
2025-06-28 13:05:19 -0400

    base funcional

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
+
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'login', loadComponent: () => import('./components/auth-component/auth-component').then(m => m.AuthComponent) }
+];
---
+++ src/app/modules/eventos/components/historical-component/historical-component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-historical-component',
+  imports: [],
+  templateUrl: './historical-component.html',
+  styleUrl: './historical-component.css'
+})
+export class HistoricalComponent {
+
+}
